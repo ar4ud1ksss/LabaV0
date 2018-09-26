c15120b414da6b11740ffa7c49c9e7b7c336faa8
Kirill 'Ar4ud1ksss' Shcherbina <43553831+ar4ud1ksss@users.noreply.github.com>
2018-09-26 16:46:43 +0300

    Add files via upload

--- labaV0.cpp
+++ labaV0.cpp
@@ -1,14 +1,34 @@
+/*!
+	\file
+	Program is helper with laba 1.1.1
+	
+        
+	//\details
+	It read file with data, process them and write result in file.
+	The data in the file should be stored in the format of 36 pairs of numbers of current and voltage.
+    If input or output file not fond, program print you error.
+	If the data file has error, then program print you error in the file.
+*/
+
 #include <stdio.h>
 #include <assert.h>
 
-
-const int NMeas    = 12;            //Number all measurements
-const int Square   = 10;            //Square of wire (10^(-4) ñm^2)
-const float Length1  = 20;            //Long of wire 1 (cm)
-const float Length2  = 30;            //Long of wire 2 (cm)
-const float Length3  = 50;            //Long of wire 3 (cm)
-const int MaxI     = 600;           //Max value ampermeter (mA)
-const int MaxU     = 600;           //Max value voltmeter (mV)
+                                        ///Number all measurements
+const int NMeas        = 12;            
+                                        ///Square of wire (10^(-4) Ã±m^2)
+const int Square       = 10;            
+                                        ///Long of wire 1 (cm)
+const float Length1    = 20;            
+                                        ///Long of wire 2 (cm)
+const float Length2    = 30;            
+                                        ///Long of wire 3 (cm)
+const float Length3    = 50;            
+                                        ///Max value ampermeter (mA)
+const int MaxI         = 300.01;           
+                                        ///Max value voltmeter (mV)
+const int MaxU         = 601;
+                                        ///Tolerance 
+const float Tolerance  = 1e-4;           
 
 
 int Compute (FILE* input, FILE* output, float length);
@@ -16,8 +36,7 @@ void ZeroArray (float array[], int size);
 void Read (float U[], float I[], int num, FILE* input);
 void CountResistivity (float U[], float I[], float R[], float length);
 void Print (float R[], FILE* output, float length);
-int Count (FILE* input, FILE* output);
-int Meassage (FILE* input, FILE* output);
+int Message (FILE* input, FILE* output);
 int CheckData (float U[], float I[]);
 
 int main ()
@@ -28,19 +47,28 @@ int main ()
     FILE* input  = fopen ("laba.txt",        "r");
     FILE* output = fopen ("Babka-labka.txt", "w");
 	
-    int meassage = Meassage (input, output);
+    int message = Message (input, output);
 
-    assert ((meassage == 0) || (meassage == 1));
+    assert ((message == 0) || (message == 1));
         
-    if (meassage == 1) return (1);  
+    if (message == 1) return (1);  
     
     int correct1 = Compute (input, output, Length1);
+
+    assert ((correct1 == 0) || (correct1 == 1));
+
     if (correct1 == 1) return (1);
    
     int correct2 = Compute (input, output, Length2);
+
+    assert ((correct2 == 0) || (correct2 == 1));
+
     if (correct2 == 1) return (1);
 
     int correct3 = Compute (input, output, Length3);
+    
+    assert ((correct3 == 0) || (correct3 == 1));
+    
     if (correct3 == 1) return (1);
  
 
@@ -52,15 +80,26 @@ int main ()
 
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that process data and print result 
+	\param input
+    Input file
+    \param output
+    Output file
+    \param length
+    Length of wire 
+*/
+
 int Compute (FILE* input, FILE* output, float length)
 	{
+    assert ((Length1 <= length) && (length <= Length3));
+
 	float U[NMeas], I[NMeas], R[NMeas];
 	ZeroArray (U, NMeas);
 	ZeroArray (I, NMeas);
 	ZeroArray (R, NMeas);
 	
-    if (input == NULL) return (-1);	
-    
     Read (U, I, NMeas, input);
 
     int check = CheckData (U, I);	
@@ -81,8 +120,19 @@ int Compute (FILE* input, FILE* output, float length)
 
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that zero array 
+	\param array[] 
+	Input array
+	\param size 
+	Size of array
+*/ 
+
 void ZeroArray (float array[], int size)
 	{
+    assert (size > 0);
+
 	for (int i = 0; i < size; i++)
         	{
         	assert (0 <= i && i < size);
@@ -93,8 +143,23 @@ void ZeroArray (float array[], int size)
 
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that read file 
+	\param U[]
+    Array with value of voltage
+    \param I[] 
+	Array with value of current
+	\param size 
+	Number of value
+    \param input 
+    Input file
+*/ 
+
 void Read (float U[], float I[], int size, FILE* input)
 	{
+    assert (size > 0);
+
 	for (int line = 0; line < size; line++)
 		{
 		assert ((0 <= line) && (line < size));
@@ -105,16 +170,24 @@ void Read (float U[], float I[], int size, FILE* input)
 
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that check data
+	\param U[]
+    Array with value of voltage
+    \param I[] 
+	Array with value of current
+*/ 
+
 int CheckData (float U[], float I[])
 	{	
 	for (int line = 0; line < NMeas; line++)
 		{
 		assert ((0 <= line) && (line < NMeas));
 
-		if ((U[line] <= 0) || (I[line] <= 0) || (U[line] > MaxU) || (I[line] > MaxI))
-			{
-            
-			return (line+1);
+		if ((U[line] - Tolerance <= 0) || (I[line] - Tolerance <= 0) || (U[line] > MaxU - Tolerance) || (I[line] > MaxI - Tolerance))
+			{  
+			return (1);
 			}
         }
 		
@@ -123,8 +196,23 @@ int CheckData (float U[], float I[])
     
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that compute resistivity 
+	\param U[]
+    Array with value of voltage
+    \param I[] 
+	Array with value of current
+    \param R[] 
+	Array with value of resistivity	
+    \param length 
+	Long of wire
+*/ 
+
 void CountResistivity (float U[], float I[], float R[], float length)
 	{
+    assert ((Length1 <= length) && (length <= Length3));
+
 	for (int i = 0; i < NMeas; i++)
 		{
 		assert ((0 <= i) && (i < NMeas));
@@ -139,15 +227,27 @@ void CountResistivity (float U[], float I[], float R[], float length)
 
 //=============================================================================
 
+/*!
+	\brief
+	Funcion that print result 
+    \param R[] 
+	Array with value of resistivity
+    \param output
+    Output file	
+    \param length 
+	Long of wire
+*/ 
+
 void Print (float R[], FILE* output, float length) 
 	{
+    assert ((Length1 <= length) && (length <= Length3));
+
     fprintf (output,"\nValue resistivity for %3.0fcm\n", length);
 	for (int i = 0; i < NMeas; i++)
 		{
 		assert ((0 <= i) && (i < NMeas));
 		
-
-		if ((i + 1) < 10)	fprintf (output,"%d.  %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
+        if ((i + 1) < 10)	fprintf (output,"%d.  %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
 				
 		else			    fprintf (output,"%d. %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
                         
@@ -156,7 +256,16 @@ void Print (float R[], FILE* output, float length)
 
 //=============================================================================
 
-int Meassage (FILE* input, FILE* output)
+/*!
+	\brief
+	Funcion that print ERROR if files weren't opened
+    \param input
+    Input file
+    \param output
+    Output file
+*/ 
+
+int Message (FILE* input, FILE* output)
     {
     if ((input == NULL) && (output == NULL))
         {
