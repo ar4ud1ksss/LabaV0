02ffa79d1a158c04e645131a20ef311e77074932
Kirill 'Ar4ud1ksss' Shcherbina <43553831+ar4ud1ksss@users.noreply.github.com>
2018-09-26 18:16:02 +0300

    Update labaV0.cpp

--- labaV0.cpp
+++ labaV0.cpp
@@ -71,8 +71,8 @@ int main ()
     
     if (correct3 == 1) return (1);
  
-
-	printf ("Result saved in file 'Babka-labka.txt'\n");
+    printf ("Result saved in file 'Babka-labka.txt'\n");
+    
     
     return (0);
     	
@@ -81,9 +81,9 @@ int main ()
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that process data and print result 
-	\param input
+    \brief
+    Funcion that process data and print result 
+    \param input
     Input file
     \param output
     Output file
@@ -92,13 +92,13 @@ int main ()
 */
 
 int Compute (FILE* input, FILE* output, float length)
-	{
+    {
     assert ((Length1 <= length) && (length <= Length3));
 
-	float U[NMeas], I[NMeas], R[NMeas];
-	ZeroArray (U, NMeas);
-	ZeroArray (I, NMeas);
-	ZeroArray (R, NMeas);
+    float U[NMeas], I[NMeas], R[NMeas];
+    ZeroArray (U, NMeas);
+    ZeroArray (I, NMeas);
+    ZeroArray (R, NMeas);
 	
     int checkread = Read (U, I, NMeas, input);
     
@@ -114,41 +114,41 @@ int Compute (FILE* input, FILE* output, float length)
 
     assert (check >= 0);
 
-	if (check > 0)
+    if (check > 0)
         {
         printf ("ERROR in data\n");
         return (1); 
         }
 		
-	CountResistivity (U, I, R, length);
-	Print (R, output, length);
+    CountResistivity (U, I, R, length);
+    Print (R, output, length);
 	
-	return (0);
+    return (0);
 
-	}
+    }
 
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that zero array 
-	\param array[] 
-	Input array
-	\param size 
-	Size of array
+    \brief
+    Funcion that zero array 
+    \param array[] 
+    Input array
+    \param size 
+    Size of array
 */ 
 
 void ZeroArray (float array[], int size)
-	{
+    {
     assert (size > 0);
 
-	for (int i = 0; i < size; i++)
-        	{
-        	assert (0 <= i && i < size);
+    for (int i = 0; i < size; i++)
+        {
+        assert (0 <= i && i < size);
 
-        	array[i] = 0;
-		}
+        array[i] = 0;
 	}
+    }
 
 //=============================================================================
 
@@ -158,121 +158,117 @@ void ZeroArray (float array[], int size)
     \param U[]
     Array with value of voltage
     \param I[] 
-	Array with value of current
-	\param size 
-	Number of value
+    Array with value of current
+    \param size 
+    Number of value
     \param input 
     Input file
 */ 
 
 int Read (float U[], float I[], int size, FILE* input)
-	{
+    {
     assert (size > 0);
 
-	for (int line = 0; line < size; line++)
-		{
-		assert ((0 <= line) && (line < size));
+    for (int line = 0; line < size; line++)
+	{
+	assert ((0 <= line) && (line < size));
 
-		int check = fscanf (input, "%f %f", &U[line], &I[line]);
+        int check = fscanf (input, "%f %f", &U[line], &I[line]);
     
         if (check != 2) return (1);
          
-		}
-    return (0);
 	}
+    return (0);
+    }
 
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that check data
-	\param U[]
+    \brief
+    Funcion that check data
+    \param U[]
     Array with value of voltage
     \param I[] 
-	Array with value of current
+    Array with value of current
 */ 
 
 int CheckData (float U[], float I[])
-	{	
-	for (int line = 0; line < NMeas; line++)
-		{
-		assert ((0 <= line) && (line < NMeas));
+    {	
+    for (int line = 0; line < NMeas; line++)
+        {
+	assert ((0 <= line) && (line < NMeas));
 
-		if ((U[line] <= 0) || (I[line] <= 0) || (U[line] > MaxU ) || (I[line] > MaxI ))
-			{  
+	if ((U[line] < 0) || ((I[line] > 0.0 - Tolerance) && (I[line] < 0.0 + Tolerance)) || (U[line] > MaxU ) || (I[line] > MaxI ))
+	    {  
             printf ("%f", U[line]);
-			return (1);
-			}
+	    return (1);
+	    }
         }
-		
-        return (0);
-	}
+
+    return (0);
+    }
     
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that compute resistivity 
-	\param U[]
+    \brief
+    Funcion that compute resistivity 
+    \param U[]
     Array with value of voltage
     \param I[] 
-	Array with value of current
+    Array with value of current
     \param R[] 
-	Array with value of resistivity	
+    Array with value of resistivity	
     \param length 
-	Long of wire
+    Long of wire
 */ 
 
 void CountResistivity (float U[], float I[], float R[], float length)
-	{
+    {
     assert ((Length1 <= length) && (length <= Length3));
 
-	for (int i = 0; i < NMeas; i++)
-		{
-		assert ((0 <= i) && (i < NMeas));
+    for (int i = 0; i < NMeas; i++)
+        {
+	assert ((0 <= i) && (i < NMeas));
 
-			{
-			R[i] = U[i] * Square/(I[i] * length);
-			}
-		
-
-		}
-	}
+	R[i] = U[i] * Square/(I[i] * length);
+        }
+    }
 
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that print result 
+    \brief
+    Funcion that print result 
     \param R[] 
-	Array with value of resistivity
+    Array with value of resistivity
     \param output
     Output file	
     \param length 
-	Long of wire
+    Long of wire
 */ 
 
 void Print (float R[], FILE* output, float length) 
-	{
+    {
     assert ((Length1 <= length) && (length <= Length3));
 
     fprintf (output,"\nValue resistivity for %3.0fcm\n", length);
-	for (int i = 0; i < NMeas; i++)
-		{
-		assert ((0 <= i) && (i < NMeas));
+    for (int i = 0; i < NMeas; i++)
+	{
+	assert ((0 <= i) && (i < NMeas));
 		
-        if ((i + 1) < 10)	fprintf (output,"%d.  %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
+        if ((i + 1) < 10)    fprintf (output,"%d.  %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
 				
-		else			    fprintf (output,"%d. %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
+	else    fprintf (output,"%d. %5.2f *10^(-4) Om*cm^2\n",i + 1, R[i]);
                         
-		}
 	}
+    }
 
 //=============================================================================
 
 /*!
-	\brief
-	Funcion that print ERROR if files weren't opened
+    \brief
+    Funcion that print ERROR if files weren't opened
     \param input
     Input file
     \param output
